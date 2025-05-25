0056b22f994fafba426d134a4ab1d7a32befa0f6
Wasp-stdnt <st5353320@stud.duikt.edu.ua>
2025-05-25 20:00:58 +0300

    Add initial project skeleton

---
+++ src/main/java/io/github/wasp_stdnt/passwordmanagerv2/PasswordManagerV2Application.java
@@ -0,0 +1,13 @@
+package io.github.wasp_stdnt.passwordmanagerv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordManagerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordManagerV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/wasp_stdnt/passwordmanagerv2/PasswordManagerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wasp_stdnt.passwordmanagerv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordManagerV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
